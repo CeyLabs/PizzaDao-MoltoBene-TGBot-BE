6d17ceabe458ef6a9a5425d8b3021e807d2faae4
mrcentimetre <udananimsara@gmail.com>
2025-05-08 12:51:52 +0530

    feat: add region, country, and city seeders for database initialization

---
+++ src/fixtures/4_user-fixtures.ts
@@ -0,0 +1,51 @@
+import { Knex } from 'knex';
+
+const tableName = 'user';
+
+export async function seed(knex: Knex): Promise<void> {
+  const [{ count }] = await knex(tableName).count();
+  if (Number(count) > 0) return;
+
+  // Fetch the country ID for "Sri Lanka"
+  interface Country {
+    id: number;
+    name: string;
+  }
+  const country: Country | undefined = await knex<Country>('country')
+    .where({ name: 'Sri Lanka' })
+    .first();
+
+  interface City {
+    id: number;
+    name: string;
+  }
+  if (!country) {
+    throw new Error('Country data is missing. Please seed it first.');
+  }
+
+  // Fetch the city ID for "Colombo"
+  const city: City | undefined = await knex<City>('city')
+    .where('name', 'Colombo')
+    .andWhere('country_id', country.id)
+    .first();
+
+  if (!country || !city) {
+    throw new Error('Country or City data is missing. Please seed them first.');
+  }
+
+  await knex(tableName).insert([
+    {
+      telegram_id: '1180327057',
+      username: 'Nimsara',
+      tg_first_name: 'Nimsara',
+      tg_last_name: '',
+      custom_full_name: 'Udana Nimsara',
+      country_id: country.id,
+      city_id: city.id,
+      role: 'admin',
+      mafia_movie: 'The Godfather',
+      ninja_turtle_character: 'Leonardo',
+      pizza_topping: 'Pepperoni',
+    },
+  ]);
+}
