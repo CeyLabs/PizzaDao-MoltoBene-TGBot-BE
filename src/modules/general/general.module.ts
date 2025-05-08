cb67dca51a750500a667f7c6b6d8a100ffa12804
mrcentimetre <udananimsara@gmail.com>
2025-05-08 11:57:32 +0530

    refactor: remove GeneralModule and GeneralService due to redundancy

--- src/modules/general/general.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GeneralService } from './general.service';
-import { UsersService } from '../users/users.service';
-
-@Module({
-  providers: [GeneralService, UsersService],
-})
-export class GeneralModule {}
