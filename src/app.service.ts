62223916200b72a9d1376bcd832d7eb78fdf8a14
mrcentimetre <udananimsara@gmail.com>
2025-05-10 12:05:17 +0530

    refactor: rename helpCommand to handleHelpCommand and update help message format

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Hears, Help, Update } from 'nestjs-telegraf';
+import { Help, Update } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 @Update()
 @Injectable()
 export class AppService {
   @Help()
-  async helpCommand(ctx: Context) {
-    await ctx.reply('Send me a sticker');
-  }
-
-  @Hears('hi')
-  async hearsHi(ctx: Context) {
-    await ctx.reply('Hey there');
+  async handleHelpCommand(ctx: Context) {
+    await ctx.replyWithMarkdownV2(
+      'ℹ️ *Help Menu*\n\n' +
+        'Here are the commands you can use:\n\n' +
+        '1\\. `/register` \\- Start the registration process\\.\n' +
+        '2\\. `/profile` \\- View your profile information\\.\n' +
+        '3\\. `/help` \\- Show this help menu\\.\n\n' +
+        'If you have any questions or need further assistance, feel free to reach out\\!',
+    );
   }
 }
