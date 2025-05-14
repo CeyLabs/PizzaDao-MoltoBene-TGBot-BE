ef0403a4384288a834bb3ad78c4c64ff2b4f198e
Dilshan Madushanka <helloscoopz@gmail.com>
2025-05-14 18:28:29 +0530

    feat: Add access table

--- src/migrations/20250507161633_create_user_table.ts
+++ src/migrations/20250507161633_create_user_table.ts
@@ -4,14 +4,13 @@ const tableName = 'user';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable(tableName, (table) => {
-    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()')); // TODO: remove this, make telegram_id primary key
     table.string('telegram_id').notNullable().unique();
     table.string('username');
     table.string('tg_first_name').notNullable();
     table.string('tg_last_name');
     table.string('pizza_name');
     table.string('discord_name');
-    table.string('role').defaultTo('user');
     table.string('mafia_movie');
     table.specificType('ninja_turtle_character', 'text[]');
     table.string('pizza_topping');
