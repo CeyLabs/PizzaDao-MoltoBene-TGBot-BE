7f17372f522e6ef28135acf60921ee97a8fe73a5
Dilshan Madushanka <helloscoopz@gmail.com>
2025-05-19 21:28:08 +0530

    chore: Fix lint error

--- src/migrations/20250512165419_create_event_details_table.ts
+++ src/migrations/20250512165419_create_event_details_table.ts
@@ -5,19 +5,24 @@ const tableName = 'event_detail';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable(tableName, (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.string('group_id').notNullable().references('group_id').inTable('city').onDelete('CASCADE');
-    table.boolean('is_one_person').notNullable().defaultTo(true),
-    table.string('name')
-    table.string('slug')
-    table.string('image_url')
-    table.string('start_date')
-    table.string('start_time')
-    table.string('end_date')
-    table.string('end_time')
-    table.string('timezone')
-    table.string('address')
-    table.string('location')
-    table.integer('year')
+    table
+      .string('group_id')
+      .notNullable()
+      .references('group_id')
+      .inTable('city')
+      .onDelete('CASCADE');
+    table.boolean('is_one_person').notNullable().defaultTo(true);
+    table.string('name');
+    table.string('slug');
+    table.string('image_url');
+    table.string('start_date');
+    table.string('start_time');
+    table.string('end_date');
+    table.string('end_time');
+    table.string('timezone');
+    table.string('address');
+    table.string('location');
+    table.integer('year');
   });
 }
 
