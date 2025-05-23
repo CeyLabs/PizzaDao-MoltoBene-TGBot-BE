7354b7e76959af4960a9e331afc4021fb3953cc7
Dilshan Madushanka <helloscoopz@gmail.com>
2025-05-23 16:28:02 +0530

    docs: update city module

--- src/modules/city/city.module.ts
+++ src/modules/city/city.module.ts
@@ -1,7 +1,18 @@
+/**
+ * @fileoverview City module for managing city-related functionality
+ * @module city.module
+ */
+
 import { Module } from '@nestjs/common';
 import { CityService } from './city.service';
 import { KnexModule } from '../knex/knex.module';
 
+/**
+ * Module for managing city data and operations
+ * @class CityModule
+ * @description Handles city-related operations, including retrieving
+ * cities by country and managing city data
+ */
 @Module({
   imports: [KnexModule],
   providers: [CityService],
