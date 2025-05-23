ed6a3ee3bde679dd486b64edb981617fe82a739e
Dilshan Madushanka <helloscoopz@gmail.com>
2025-05-23 16:23:06 +0530

    docs: update user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,19 @@
+/**
+ * @fileoverview User module for managing user-related functionality
+ * @module user.module
+ */
+
 import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { KnexModule } from '../knex/knex.module';
 import { UserController } from './user.controller';
 
+/**
+ * Global module for user management
+ * @class UserModule
+ * @description Provides user-related functionality across the application
+ * @decorator @Global() - Makes this module available globally
+ */
 @Global()
 @Module({
   imports: [KnexModule],
