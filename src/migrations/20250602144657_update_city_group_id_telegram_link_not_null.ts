332d556fda3eb4f4f8ca2521c5261f2187e99457
Dilshan Madushanka <helloscoopz@gmail.com>
2025-06-04 00:31:40 +0530

    fix: make city.telegram_link notNullable

--- src/migrations/20250602144657_update_city_group_id_telegram_link_not_null.ts
+++ src/migrations/20250602144657_update_city_group_id_telegram_link_not_null.ts
@@ -5,11 +5,13 @@ const tableName = 'city';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable(tableName, (table) => {
     table.string('group_id').notNullable().alter();
+    table.string('telegram_link').notNullable().alter();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable(tableName, (table) => {
     table.string('group_id').nullable().alter();
+    table.string('telegram_link').nullable().alter();
   });
 }
